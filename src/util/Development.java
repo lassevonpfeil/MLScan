a228cc2b992448f9dda4d586627ed436a2c3b6eb
lassevonpfeil <lasse.v.p.123@gmail.com>
2025-08-16 21:38:10 +0200

    simplified

--- src/util/Development.java
+++ src/util/Development.java
@@ -4,7 +4,7 @@ public class Development {
     static boolean development = true;
 
     public static String isInDevelopment() {
-        if (Development.development == true) {
+        if (Development.development) {
             return " (dev version)";
         } else {
             return "";
