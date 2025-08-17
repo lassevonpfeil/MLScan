add005c90574cb93dec2fa114af695e40befa058
lassevonpfeil <lasse.v.p.123@gmail.com>
2025-08-17 12:18:08 +0200

    implemented console label

--- src/ui/Console.java
+++ src/ui/Console.java
@@ -1,7 +1,33 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Console {
-    static JTextField console = new JTextField();
+    JLabel label = new JLabel();
+
+    public Console() {
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setBounds(30, 190, 800, 30);
+        label.setOpaque(true);
+        label.setBackground(Color.LIGHT_GRAY);
+        label.setVisible(true);
+        // log("Console label", true, false);
+    }
+
+    public void log(String message, boolean error, boolean finish) {
+        label.setText(message);
+
+        if (finish) {
+            if (error) {
+                label.setBackground(Color.RED);
+            } else {
+                label.setBackground(Color.GREEN);
+            }
+        } else {
+            label.setBackground(Color.LIGHT_GRAY);
+        }
+    }
 }
